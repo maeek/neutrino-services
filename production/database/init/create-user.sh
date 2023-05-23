c09100e2d31669e489fb35564bb70a823a2d8be4
maeek <maciej@suchanecki.me>
2023-05-23 23:19:34 +0200

    Max 80 lines width

--- production/database/init/create-user.sh
+++ production/database/init/create-user.sh
@@ -6,4 +6,8 @@ mongosh ${MONGO_INITDB_DATABASE} \
         -u ${MONGO_INITDB_ROOT_USERNAME} \
         -p ${MONGO_INITDB_ROOT_PASSWORD} \
         --authenticationDatabase admin \
-        --eval "db.createUser({user: '${MONGO_USER}', pwd: '${MONGO_PASSWORD}', roles:[{role:'readWrite', db: '${MONGO_INITDB_DATABASE}'}]});"
+        --eval "db.createUser({ \
+          user: '${MONGO_USER}', \
+          pwd: '${MONGO_PASSWORD}', \
+          roles:[{role:'readWrite', db: '${MONGO_INITDB_DATABASE}'}] \
+        });"
